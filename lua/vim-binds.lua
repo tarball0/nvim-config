d867f10cb9c4806d55eea08e7056e7a2c0506836
tarball0 <chrispaul.tar.gz@proton.me>
2024-10-24 14:19:36 +0530

    modified vim keybinds

--- lua/vim-binds.lua
+++ lua/vim-binds.lua
@@ -9,8 +9,7 @@ vim.cmd("autocmd filetype cpp nnoremap <C-c> :!g++ -o  %:r % -pedantic-errors -W
 vim.cmd("autocmd filetype asm nnoremap <C-c> :!nasm -f elf32 % -o %:r.o<Enter>")
 vim.cmd("autocmd filetype asm nnoremap <C-l> :!ld -m elf_i386 %:r.o -o %:r ")
 vim.cmd("autocmd filetype java nnoremap <C-c> :!javac %<Enter>")
-vim.cmd("autocmd filetype java nnoremap <C-x> :term java ")
+vim.cmd("autocmd filetype java nnoremap <C-x> :term java Main")
 vim.cmd("nnoremap <C-x> :term %:p:r")
 vim.cmd("nnoremap <F3> :!python3 %<Enter>")
-vim.cmd("nnoremap <C-i> :!clear<Enter>")
 vim.g.mapleader = " "
