932eb25060fe9718d5184fd6bfa30797ca4d662d
tarball0 <chrispaul.tar.gz@proton.me>
2024-11-12 17:37:04 +0530

    changes to keybinds and other basic things

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -26,6 +26,13 @@ return {
       [[                                                                       ]],
     }
 
+	dashboard.section.buttons.val = {
+    dashboard.button( "Ctrl f", "   Show Files", ":Telescope find_files<CR>"),
+    dashboard.button( "e", "   New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "f", "󰈞   Find file", ":Telescope find_files<CR>"),
+    dashboard.button( "r", "   Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "q", "󰈆   Quit NVIM", ":qa<CR>"),
+}
     alpha.setup(dashboard.opts)
   end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,8 +5,10 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fh", builtin.oldfiles, {})
+			vim.keymap.set("n", "<leader>fm", builtin.marks, {})
 		end,
 	},
 
@@ -14,6 +16,9 @@ return {
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = function()
 			require("telescope").setup({
+				defaults = {
+					initial_mode = "normal",
+				},
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({}),
