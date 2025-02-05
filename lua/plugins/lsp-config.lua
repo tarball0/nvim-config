0d4728caa777961728b15f6d0f33ccd257b0c377
tarball0 <chrispaul.tar.gz@proton.me>
2025-02-05 09:18:10 +0530

    added bash lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -44,6 +44,9 @@ return {
 			lspconfig.golangci_lint_ls.setup({
 				capabilities = capabilities,
 			})
+			lspconfig.bashls.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
