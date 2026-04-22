da36aceb56d89dbddb1cd375a9477c93ca04f400
tarball0 <chrispaul.y2k5@gmail.com>
2026-04-23 03:13:35 +0530

    added more opencode shortcuts

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -27,6 +27,20 @@ return {
   config = function()
     ---@type opencode.Opts
     vim.g.opencode_opts = {
+      server = {
+        start = function()
+          require("opencode.terminal").open("opencode --port --agent plan", {
+            split = "right",
+            width = math.floor(vim.o.columns * 0.35),
+          })
+        end,
+        toggle = function()
+          require("opencode.terminal").toggle("opencode --port --agent plan", {
+            split = "right",
+            width = math.floor(vim.o.columns * 0.35),
+          })
+        end,
+      },
       -- Your configuration, if any; goto definition on the type or field for details
     }
 
@@ -36,6 +50,7 @@ return {
     vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
     vim.keymap.set({ "n", "x" }, "<C-,>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
     vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+    vim.keymap.set({ "n", "t" }, "<C-t>", function() require("opencode").command("agent.cycle") end,            { desc = "Cycle opencode agent" })
 
     vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
     vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
